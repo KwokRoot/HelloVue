6d7c2ae0ac83cf14410663d03f5105f6bba374e2
KwokRoot <KwokRoot@users.noreply.github.com>
2020-06-21 13:58:15 +0800

    Add TodoList Test Project.
    1.add TodoList Test Project.

---
+++ todolist/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <input v-model="inputValue" placeholder="todo" />
+    <button @click="addTask">提交</button>
+    <ul>
+      <todoItem
+        v-for="(item, index) in taskList"
+        :key="index"
+        :item="item"
+        :index="index"
+        @deltask="delTask"
+      ></todoItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todoItem from "./components/todoItem.vue";
+
+export default {
+  name: "App",
+  components: {
+    todoItem,
+  },
+  data: function() {
+    return { inputValue: "", taskList: [] };
+  },
+  methods: {
+    addTask: function() {
+      this.taskList.push(this.inputValue);
+      this.inputValue = "";
+    },
+    delTask: function(index) {
+      this.taskList.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ todolist/src/components/todoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>{{ item }}&nbsp;<span class="del" @click="del">×</span></li>
+</template>
+
+<script>
+export default {
+  name: "todoItem",
+  props: ["item", "index"],
+  methods: {
+    del: function() {
+      this.$emit("deltask", this.index);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.del {
+  color: red;
+  cursor: pointer;
+}
+</style>
